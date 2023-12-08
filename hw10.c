fc779a9a0a13f338f1cbbd6d8e99ab033b93c669
leegoeun-art <127366729+leegoeun-art@users.noreply.github.com>
2023-12-08 16:35:36 +0900

    Update hw10.c

--- hw10.c
+++ hw10.c
@@ -1,11 +1,18 @@
-#include <stdio.h>
+도시의 이름, 속한 국가, 인구 수에 대한 정보를 저장할 수 있는 간단한 구조체를 정의하고, 구조체
+배열을 선언해서 이러한 도시에 대한 정보를 저장하는 구조로 작성하세요. 메인 함수에는 사용자
+로부터 3개 도시에 대한 정보를 입력 받고, 입력이 끝나면 이 도시에 대한 내용을 출력해 주세요.
+hw10.c 파일을 만들어 개인 github 리파지토리에 올려 주시기 바랍니다. (기한: 다음 수업시간 전)
 
-// 도시 정보를 저장하는 구조체
-struct City {
-    char name[100];
-    char country[100];
+#include <stdio.h>
+#include <string.h>
+#define MAX_STRING_SIZE 30 //코드할 때 MAX 써있는 거 30으로 다 바뀜 
+    
+typedef struct city
+{
+    char name[MAX_STRING_SIZE]
+    char country[MAX_STRING_SIZE]
     int population;
-};
+}city;
 
 int main() {
     // 도시 정보를 저장하는 구조체 배열 선언
@@ -15,10 +22,10 @@ int main() {
     printf("Input three cities:\n");
     for (int i = 0; i < 3; ++i) {
         printf("Name> ");
-        scanf("%s", cities[i].name);
+        fgets(city_list[i].name, MAX_STRING_SIZE, stdin);
 
         printf("Country> ");
-        scanf("%s", cities[i].country);
+        fgets(city_list[i].name, MAX_STRING_SIZE, stdin);
 
         printf("Population> ");
         scanf("%d", &cities[i].population);
