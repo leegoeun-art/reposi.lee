0a9346b4b895c52a6abdef8d77eed64d28fb7b66
leegoeun-art <127366729+leegoeun-art@users.noreply.github.com>
2023-12-22 14:56:18 +0900

    Update hw10.c

--- hw10.c
+++ hw10.c
@@ -46,3 +46,57 @@ int main() {
 
     return 0;
 }
+
+
+
+이게 진짜 답 같은데..
+#include <stdio.h>
+#include <string.h>
+#define MAX_STRING_SIZE 50 //코드할 때 MAX 써있는 거 30으로 다 바뀜 
+
+typedef struct city
+{
+    char name[MAX_STRING_SIZE]
+    char country[MAX_STRING_SIZE]
+    int population;
+} City;
+
+int main(void)
+{
+    City city_list[3] = { 0 };
+    int i;
+
+    printf("Input three cities:\n");
+    for (i = 0; i<3; i++)
+    {
+        printf("Name> ");
+        fgets(city_list[i].name, population);
+
+        printf("Input three cities:\n");
+        for (int i = 0; i < 3; ++i) {
+        
+        printf("Name> ");
+        fgets(city_list[i].name, MAX_STRING_SIZE, stdin);
+
+        printf("Country> ");
+        fgets(city_list[i].name, MAX_STRING_SIZE, stdin);
+
+        printf("Population> ");
+        scanf("%d", &(city_list[i].population));
+
+        while (getchar() != '\n');
+    }
+    printf("\n");
+
+    printf("Printing the three cities:\n");
+    for (i = 0; i < 3; i++)
+    {
+        city_list[i].name[strlen(city_list[i].name) - 1] = 0;
+        city_list[i].country[strlen(city_list[i].country) - 1] = 0;
+        printf("%d. %s in %s with a population of %d people \n",
+            i + 1, city_list[i].name, city_list[i].country, city_list[i], population);
+    }
+
+    return 0;
+}
+
